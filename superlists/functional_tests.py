fe4478842c7ed4a263c93c9189317e6259256fc6
Phil Wells <philwells@thephilwells.com>
2017-11-25 13:51:50 -0500

    add reqs to e2e test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,42 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_star_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app.
+        # he goes to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('FInish the test!')
+
+        # She is invited to enter a to-do item right away
+
+        # She types "buy peacock feather" into a text box
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" an item in a to-do list
+
+        # There is still a text box inviting her to add other items
+        # She enters "Use peacock feather to make a fly"
+
+        # The age updates again, and now shows both items on her list
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her - there is some
+        # explanatory text to that effect
+
+        # She visits that url - her to-do list is still there
+
+        # Satisfied, she goes back to sleep
+
+if __name__ == '__main__':
+    unittest.main()
